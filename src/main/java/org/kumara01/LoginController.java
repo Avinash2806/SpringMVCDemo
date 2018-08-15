175a020a5efb3366a14e8d3e7e50b9105b48ad52
KumarA01 <amrendra92@ymail.com>
2018-08-15 17:22:29 +0530

    Initial Commit

---
+++ src/main/java/org/kumara01/LoginController.java
@@ -0,0 +1,22 @@
+package org.kumara01;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+public class LoginController {
+ 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("message", "Hello World");
+        return "welcom";
+    }
+ 
+    @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("message", "Hello World Again");
+        return "welcom";
+    }
+ 
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+	<form action="login">
+	<input type="submit"
+			value="login" />
+	</form>
+</body>
+
+</html>
